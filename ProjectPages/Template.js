4b38b111aa757591e409497925dbf5d551cb2cea
NightGaul <116014631+NightGaul@users.noreply.github.com>
2026-02-23 23:19:49 +0900

    First draft

---
+++ ProjectPages/Template.js
@@ -0,0 +1,20 @@
+const url = 'Project1.json'
+
+// Function to fetch data and update HTML
+async function fetchAndDisplayProducts() {
+
+    // Fetch the JSON file
+    const response = await fetch(url);
+
+    const attributes = await response.json();
+
+    document.getElementById('title').innerHTML = attributes["Title"];
+    document.getElementById('role').innerHTML = attributes["Role"];
+    document.getElementById('team').innerHTML = attributes["Team"];
+    document.getElementById('description').innerHTML = attributes["Description"];
+    document.getElementById('reflection').innerHTML = attributes["Reflection"];
+    document.getElementById('focus').innerHTML = attributes["Focus"]
+}
+
+// Call the function to display the data
+fetchAndDisplayProducts();
